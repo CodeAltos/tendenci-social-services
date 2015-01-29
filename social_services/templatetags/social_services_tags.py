751ed1a2e05535eef23ca67876f8deee230d7b85
Max <maxc@bixly.com>
2015-01-29 12:14:29 +0800

    added updates to include megamenu to social services module T5 version

--- social_services/templatetags/social_services_tags.py
+++ social_services/templatetags/social_services_tags.py
@@ -10,3 +10,11 @@ def profile_result(context, profile):
         'profile': profile,
     })
     return context
+
+@register.inclusion_tag("social_services/top_nav_items.html", takes_context=True)
+def social_service_current_app(context, user, social_service=None):
+    context.update({
+        'app_object': social_service,
+        "user": user
+    })
+    return context
